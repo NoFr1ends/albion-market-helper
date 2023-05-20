47141c222bc21812dcce8443c2dcf2693f328036
Lynx <lynx@krypton.dev>
2023-05-20 13:38:09 +0200

    Change theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/md-dark-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import './index.css';
 import App from './App';
